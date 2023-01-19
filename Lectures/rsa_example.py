ff3b63a0e9a79da531acd28c9269405bfae0d56a
Andreas Nilsson Ström <andreas.nilsson@natrolit.se>
2023-01-19 10:35:45 +0100

    Update RSA example with better printing of encrypted text

--- Lectures/rsa_example.py
+++ Lectures/rsa_example.py
@@ -1,13 +1,29 @@
 import rsa
+import base64
 
 if __name__ == '__main__':
+    # Generate a new public and private key-pair
     pubkey, privkey = rsa.newkeys(1024)
     # print(pubkey.save_pkcs1().decode('utf-8'))
     # print(privkey.save_pkcs1().decode('utf-8'))
     # pubkey.pem
     # privkey.pem
 
+    # The original message
     message = 'secret message!'
+
+    # Encrypt the text
     crypto_text = rsa.encrypt(bytes(message, 'utf-8'), pubkey)
-    print('Encrypted: ', crypto_text)
-    print('Decrypted: ', rsa.decrypt(crypto_text, privkey).decode('utf-8'))
+    # Store encrypted text using base64 encoding to make the text more compact and easier on the eyes
+    b64_crypto_text = base64.b64encode(crypto_text).decode('utf-8')
+
+    # Decrypt the text
+    # Either decrypt the regular crypto text
+    decrypted_message = rsa.decrypt(crypto_text, privkey).decode('utf-8')
+    # Or decrypt the base64-coded crypto text
+    plain_crypto_text = base64.b64decode(bytes(b64_crypto_text, 'utf-8'))
+    decrypted_b64_msg = rsa.decrypt(plain_crypto_text, privkey).decode('utf-8')
+
+    # Print
+    print('Encrypted: ', b64_crypto_text)
+    print('Decrypted: ', decrypted_b64_msg)
